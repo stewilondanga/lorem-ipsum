044981b9b9ae92c7569d8cf2ede0da84249eebf4
Stewart ilondanga <stilondanga@gmail.com>
2021-04-27 11:40:23 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,23 @@
 var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
+
+// Fetch all details element
+const details = Array.from(document.querySelectorAll("details"));
+
+// Add onclick listeners
+details.forEach(targetDetail => {
+  targetDetail.addEventListener("click", () => {
+    // Close all details that are not targetDetail
+    details.forEach(detail => {
+      if (detail !== targetDetail) {
+        detail.removeAttribute("open");
+      }
+    });
+  });
+});
